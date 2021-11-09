a3acb14fa0df2a07853dc783ac5b7df94d9272eb
= <=>
2021-11-09 17:28:43 +0300

    if the user is authenticated and access to login page or register redirectes to index page

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login_view/', views.login_view, name="login_view"),    
+    path('login_view/', views.login_view, name="login_view"),   
+    path('logout_view', views.logout_view, name="logout_view"), 
     path('register/', views.register, name="register"),
     path('home/', views.home, name="home"),   
     path('admin/', views.admin, name="admin"),
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 import django
 from django.shortcuts import redirect, render
 from .forms import SignUpForm, LoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -12,59 +12,74 @@ def index(request):
 
 
 def register(request):
-    msg = None
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            msg = 'User Created'
-            return redirect('login_view')
+    if not request.user.is_authenticated:
+        msg = None
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                msg = 'User Created'
+                return redirect('login_view')
+            else:
+                msg = "Form is not valid"
         else:
-            msg = "Form is not valid"
+            form = SignUpForm()
+        return render(request, 'auth/registration.html', {'form': form, 'msg': msg})
     else:
-        form = SignUpForm()
-    return render(request, 'auth/registration.html', {'form': form, 'msg': msg})
+        return redirect('index')
 
 
 def login_view(request):
-    form = LoginForm(request.POST or None)
-    msg = None
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = LoginForm(request.POST or None)
+        msg = None
 
-    if request.method == 'POST':
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None and user.is_admin:
-                login(request, user)
-                return redirect('admin')
-            elif user is not None and user.is_customer:
-                login(request, user)
-                return redirect('customer')
-            elif user is not None and user.is_employee:
-                login(request, user)
-                return redirect('employee')
+        if request.method == 'POST':
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None and user.is_admin:
+                    login(request, user)
+                    return redirect('admin')
+                elif user is not None and user.is_customer:
+                    login(request, user)
+                    return redirect('customer')
+                elif user is not None and user.is_employee:
+                    login(request, user)
+                    return redirect('employee')
+                else:
+                    msg = 'invalid credentials'
             else:
-                msg = 'invalid credentials'
-        else:
-            msg = 'error validating form'
-
+                msg = 'error validating form'
     return render(request, 'auth/login.html', {'form': form, 'msg': msg})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
 
 
 @login_required(login_url='/login_view/')
 def home(request):
-    if  request.user.is_admin:
-         return render(request, 'home.html')
+    if request.user.is_admin:
+        return render(request, 'home.html')
     else:
-        return redirect('login_view')
+        return redirect('index')
+
+
+@login_required(login_url='/login_view/')
 def admin(request):
     return render(request, 'admin.html')
 
 
+@login_required(login_url='/login_view/')
 def customer(request):
     return render(request, 'customer.html')
 
 
+@login_required(login_url='/login_view/')
 def employee(request):
     return render(request, 'employee.html')
